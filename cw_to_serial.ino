b48ee92b508753d86d831cf9681e01e15e147b4e
30 <30crows@protonmail.ch>
2021-12-21 13:19:14 +0100

    No interrupts working quite nicely

--- cw_to_serial.ino
+++ cw_to_serial.ino
@@ -1,33 +1,28 @@
 static const uint8_t in_key_pin = 7;    // D7  : interrupt 4 on pin 1
 static const uint8_t out_led_pin = 13;  // D13 : Built-in LED on pin 13
-static volatile bool key_event;
 static bool key_is_up;
-static const unsigned long debounce_millis = 10; // 30 WPM == 40 ms
-static unsigned long last_event_millis;
+static const unsigned long debounce_millis = 20; // 30 WPM == 40 ms
+static unsigned long last_change_millis;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(in_key_pin, INPUT_PULLUP);
   pinMode(out_led_pin, OUTPUT);
   key_is_up = digitalRead(in_key_pin);
-  last_event_millis = millis();
-  uint8_t irqn = digitalPinToInterrupt(in_key_pin);
-  attachInterrupt(irqn, isr_key, CHANGE);
+  Serial.print(!key_is_up);
+  last_change_millis = millis();
 }
 
 void loop() {
-  unsigned long now = millis();
-  if (!key_event) {
+  unsigned long millis_now = millis();
+  if (key_is_up == digitalRead(in_key_pin)) {
     return;
   }
-  key_event = false;
-  if (last_event_millis + debounce_millis > now) {
+  if (last_change_millis + debounce_millis > millis_now) {
     return;
   }
-  key_is_up = digitalRead(in_key_pin);
-  digitalWrite(out_led_pin, !key_is_up);
-  last_event_millis = now;
-}
-
-void isr_key() {
-  key_event = true;
+  Serial.print(key_is_up); // inverted
+  digitalWrite(out_led_pin, key_is_up); // inverted
+  key_is_up = !key_is_up;
+  last_change_millis = millis_now;
 }
